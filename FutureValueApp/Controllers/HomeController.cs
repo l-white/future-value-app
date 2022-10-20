11c4d37b49cc8f6a01bcb3efb78720fc170ee9ac
Laura White (ETS CHX US) <Laura.White@chromalox.com>
2022-10-20 09:22:29 -0500

    first commit

---
+++ FutureValueApp/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using FutureValueApp.Models;
+
+namespace FutureValueApp.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public IActionResult Index()
+    {
+        ViewBag.FV = 0;
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Index(FutureValueModel model)
+    {
+        ViewBag.FV = model.CalculateFutureValue();
+        return View(model);
+    }
+    
+    public IActionResult About()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
